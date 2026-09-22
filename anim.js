58d5bd7568f93ed41d49e2a41da5acf80d3c5578
kikimint14-lang <kikimint14@gmail.com>
2026-09-21 20:28:24 -0600

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,14 @@
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
